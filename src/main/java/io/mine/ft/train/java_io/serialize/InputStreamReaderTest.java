cd91911f6a66b80763212506c13bc7d8abdd3080
zhuchuangwang <978733153@qq.com>
2019-09-25 23:20:41 +0800

    tijie

---
+++ src/main/java/io/mine/ft/train/java_io/serialize/InputStreamReaderTest.java
@@ -0,0 +1,27 @@
+package io.mine.ft.train.java_io.serialize;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class InputStreamReaderTest {
+
+	public static void main(String[] args) {
+		method1();
+	}
+
+	private static void method1() {
+		try(
+		InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);)
+		{
+			String buffer;
+			if ((buffer = bufferedReader.readLine()) != null) {
+				System.out.println("输入内容为："+new String(buffer.getBytes(Charset.forName("UTF-8"))));
+			}
+		}catch (Exception e) {
+			
+		}
+	}
+
+}
